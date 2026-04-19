07e44710014eef6ccc01e951ee97aab6c79766ec
Den BrAndr <89998888881@mail.ru>
2026-04-19 13:02:00 +0300

    Attributes and class interactions

--- solution.py
+++ solution.py
@@ -7,6 +7,21 @@ class Student:
         self.courses_in_progress = []
         self.grades = {}
 
+    def rate_lecture(self, lecturer, course, grade):
+        if (
+            isinstance(lecturer, Lecturer) and
+            course in self.courses_in_progress and
+            course in lecturer.courses_attached and
+            1 <= grade <= 10
+        ):
+            if course in lecturer.grades:
+                lecturer.grades[course] += [grade]
+            else:
+                lecturer.grades[course] = [grade]
+        else:
+            return 'Ошибка'
+
+
 class Mentor:
     def __init__(self, name, surname):
         self.name = name
@@ -17,7 +32,7 @@ class Mentor:
 class Lecturer(Mentor):
     def __init__(self, name, surname):
         super().__init__(name, surname)
-        self.grades = {}  # Оценки от студентов
+        self.grades = {}
 
 
 class Reviewer(Mentor):
@@ -33,9 +48,19 @@ class Reviewer(Mentor):
         else:
             return 'Ошибка'
 
+
+# Тестирование
 lecturer = Lecturer('Иван', 'Иванов')
 reviewer = Reviewer('Пётр', 'Петров')
-print(isinstance(lecturer, Mentor)) # True
-print(isinstance(reviewer, Mentor)) # True
-print(lecturer.courses_attached)    # []
-print(reviewer.courses_attached)    # []
+student = Student('Алёхина', 'Ольга', 'Ж')
+
+student.courses_in_progress += ['Python', 'Java']
+lecturer.courses_attached += ['Python', 'C++']
+reviewer.courses_attached += ['Python', 'C++']
+
+print(student.rate_lecture(lecturer, 'Python', 7))   # None
+print(student.rate_lecture(lecturer, 'Java', 8))     # Ошибка
+print(student.rate_lecture(lecturer, 'С++', 8))      # Ошибка
+print(student.rate_lecture(reviewer, 'Python', 6))   # Ошибка
+
+print(lecturer.grades)  # {'Python': [7]}
